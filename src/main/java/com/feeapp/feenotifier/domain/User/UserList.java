a50ff44e4dcfbc4beb77f909ad068d7740c58333
garv wadhwa <garv.wadhwa@jupiter.money>
2021-10-09 14:50:13 +0530

    ./gradlew goJF

--- src/main/java/com/feeapp/feenotifier/domain/User/UserList.java
+++ src/main/java/com/feeapp/feenotifier/domain/User/UserList.java
@@ -1,17 +1,15 @@
 package com.feeapp.feenotifier.domain.User;
 
 import com.feeapp.feenotifier.spi.db.entity.UserEntity;
-import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
 
 @Data
 public class UserList {
-    private List<UserEntity> users;
+  private List<UserEntity> users;
 
-    public UserList() {
-        this.users = new ArrayList<>();
-    }
+  public UserList() {
+    this.users = new ArrayList<>();
+  }
 }
--- src/main/java/com/feeapp/feenotifier/spi/db/mapper/EntityToUserMapper.java
+++ src/main/java/com/feeapp/feenotifier/spi/db/mapper/EntityToUserMapper.java
@@ -4,16 +4,16 @@ import com.feeapp.feenotifier.domain.User.User;
 import com.feeapp.feenotifier.spi.db.entity.UserEntity;
 
 public class EntityToUserMapper {
-    public static User map(UserEntity userEntity) {
-        User user = new User();
-        user.setFirstName(userEntity.getFirstName());
-        user.setUserId(userEntity.getUserId());
-        user.setLastName(userEntity.getLastName());
-        user.setEmail(userEntity.getEmail());
-        user.setPhoneNumber(userEntity.getPhoneNumber());
-        user.setPassword(userEntity.getPassword());
-        user.setUserPermission("BASIC");
-        user.setIsActive(userEntity.getIsActive());
-        return user;
-    }
+  public static User map(UserEntity userEntity) {
+    User user = new User();
+    user.setFirstName(userEntity.getFirstName());
+    user.setUserId(userEntity.getUserId());
+    user.setLastName(userEntity.getLastName());
+    user.setEmail(userEntity.getEmail());
+    user.setPhoneNumber(userEntity.getPhoneNumber());
+    user.setPassword(userEntity.getPassword());
+    user.setUserPermission("BASIC");
+    user.setIsActive(userEntity.getIsActive());
+    return user;
+  }
 }
